aa54903561b969eb4ec697d10235dc94b5e4882b
Amanda <aleisero@ucsc.edu>
2018-07-09 17:20:45 -0700

    shooting stars = constellation drawing

--- stargazing.js
+++ stargazing.js
@@ -1,10 +1,9 @@
-//set a random seed to ensure that the BG stars do not move every frame
-var r = Math.random();
+//Stargazing by Amanda Leiserowitz
+//an interactive constellation-drawing program
+//the user is given a random sky to connect stars upon
+//every 10 seconds, the sky and drawings are reset
 
 function makeSky() {
-	//apply random seed
-	randomSeed(r);
-
 	//set variables
 	var numStars = 800;
 	let i = 0;
@@ -28,84 +27,27 @@ function makeSky() {
 function setup() {
 	createCanvas(640,480);
 	makeSky();
-	
 }
 
 function draw() {
-	//shooting star variables
-		var starX = mouseX;
-		var starY = mouseY;
-		var trajX = random(-1, 1);
-		var trajY = random(-1, 1);
+	//constellation object literal
+ 	var constellation = {
+		x : mouseX,
+		y : mouseY,
+		trajX : random(-1, 1),
+		trajY : random(-1, 1)
+	};
 	
 	if (mouseIsPressed) {
-		print('mouseIsPressed');
-		//draw first ellipse of shooting star
-		ellipse(starX, starY, 2, 2);
+		//draw transparent constellation connections
+		fill(255,255,255,50);
+		ellipse(constellation.x, constellation.y, 2, 2);
 	}
 	
-	//check every frame
-	if (frameCount % 90 == 0) {
-		print('frameCount % 50 == 0');
-			
-		//update star Position for next frame
-		starX+=trajX;
-		starY+=trajY;
-			
-		//redraw star
-		ellipse(starX, starY, 2,2);
-			
+	//check every frame, adapted from the following link
+	//https://forum.processing.org/one/topic/how-to-perform-an-action-every-x-seconds-time-delays.html
+	if (frameCount % 600 == 0) {
 		//redraw sky
 		makeSky();
-		}
+		}	
 }
-
-/*
-function mousePressed() {
-		//variables for shooting star
-		var starX = mouseX;
-		var starY = mouseY;
-	
-		//reset randomSeed for trajectory of each shooting star so each is different
-		//it is set to a random range
-		//randomSeed(random(34,32453));
-	
-		var trajX = random(-1, 1);
-		var trajY = random(-1, 1);
-		
-		let j = 200;
-		while (j > 0) {
-			//draw the star
-			ellipse(starX, starY, 2, 2);
-			print('draw shooting star');
-			
-			//move the star
-			starX+=trajX;
-			starY+=trajY;
-			
-			print('frameCount = ' + frameCount);
-			
-			//update the position and redraw star and BG based on frameCount
-			if (frameCount % 50 == 0) {
-				print('frameCount % 50 == 0');
-				//redraw BG
-				redraw();
-				//redraw star
-				ellipse(starX, starY, 2,2);
-			}
-			
-			//redraw the star
-			ellipse(starX, starY, 2,2);
-			
-			//increment while loop
-			j-=1;
-			
-			//redraw the BG from the draw() function
-			//redraw();
-			//print('redraw BG');
-		}
-		
-		print('mousePressed function done');
-}
-*/
-	
